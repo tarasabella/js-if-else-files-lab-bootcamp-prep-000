fb351536a617405835d4aae04fa31b371d7ffb3e
Tara Sabella <tarasabella@gmail.com>
2020-08-18 18:34:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var name = "Susan"
 var height = "74"
 
-if ( name == "Susan") {
-  console.log(True)
-
+if (name === "Susan") {
+    console.log("The name is correct")
 }
 
 // Don't worry about this
